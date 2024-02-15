bd4668d57ec1805ea10d1e23e3113f201e2fd337
MichaelScofield45 <paco.villamonte@gmail.com>
2024-02-14 19:03:04 -0600

    plugins: replace lazy with mini.deps
    This is much simpler

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,91 +1,55 @@
-return {
-    {
-        "echasnovski/mini.nvim",
-        version = false,
-        config = function()
-            require("mini.align").setup()
-            require("mini.comment").setup()
-            require("mini.completion").setup()
-            require("mini.pairs").setup()
-            require("mini.pick").setup()
-            require("mini.surround").setup()
-            require("mini.ai").setup()
-            require("mini.statusline").setup()
-            require("mini.base16").setup({
-                palette = require("mini.base16").mini_palette('#112641', '#e2e98f', 75),
-                use_cterm = true
-            })
-        end
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({
-                signcolumn = false
-            })
-        end
-    },
-    {
-        "nvim-tree/nvim-web-devicons",
-        lazy = true
-    },
-    {
-        "folke/zen-mode.nvim",
-        cmd = "ZenMode",
-        opts = {
-            window = {
-                width = 90,
-                options = {
-                    number = false,
-                    colorcolumn = "0",
+local add = MiniDeps.add
+local now = MiniDeps.now
+local later = MiniDeps.later
+
+add("echasnovski/mini.nvim")
+require("mini.align").setup()
+require("mini.comment").setup()
+require("mini.completion").setup()
+require("mini.pairs").setup()
+require("mini.pick").setup()
+require("mini.surround").setup()
+require("mini.ai").setup()
+require("mini.statusline").setup()
+require("mini.base16").setup({
+    palette = require("mini.base16").mini_palette('#112641', '#e2e98f', 75),
+    use_cterm = true
+})
+
+add({
+    source = "nvim-treesitter/nvim-treesitter",
+    hooks = { post_checkout = function() vim.cmd("TSUpdate") end },
+})
+
+add("neovim/nvim-lspconfig")
+
+add("lewis6991/gitsigns.nvim")
+require("gitsigns").setup({
+    colorcolumn = false
+})
+
+add("nvim-tree/nvim-web-devicons")
+
+add("kaarmu/typst.vim")
+
+add("nvim-lua/plenary.nvim")
+
+add({
+    source = "nvim-neorg/neorg",
+    hooks = { post_checkout = function() vim.cmd(":Neorg sync-parsers") end },
+})
+later(function()
+    require("neorg").setup({
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.ui.calendar"] = {},
+        ["core.dirman"] = {
+            config = {
+                workspaces = {
+                    semester = "~/Documents/Feb-Jun2023",
+                    summer = "~/Documents/EstudioVerano"
                 }
-            },
-            plugins = {
-                options = {
-                    cmdheight = 1
-                }
-            }
-        }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = true
-    },
-    {
-        "neovim/nvim-lspconfig",
-    },
-    {
-        "nvim-lua/plenary.nvim",
-    },
-    {
-        "nvim-neorg/neorg",
-        ft = "norg",
-        cmd = "Neorg",
-        build = ":Neorg sync-parsers",
-        opts = {
-            load = {
-                ["core.defaults"] = {},
-                ["core.concealer"] = {},
-                ["core.ui.calendar"] = {},
-                ["core.dirman"] = {
-                    config = {
-                        workspaces = {
-                            semester = "~/Documents/Feb-Jun2023",
-                            summer = "~/Documents/EstudioVerano"
-                        }
-                    }
-                },
-                ["core.completion"] = {
-                    config = {
-                        engine = "nvim-cmp",
-                    },
-                },
             }
         },
-    },
-    {
-        "kaarmu/typst.vim",
-        ft = "typst"
-    },
-}
+    })
+end)
--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,10 +1,10 @@
--- Neorg setup to work with treesitter
--- local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { "norg", "cpp", "c", "javascript", "python", "lua", "zig", "gitcommit", "git_rebase", "rust" },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
+-- NOTE: I don't know if this is even useful
+MiniDeps.later(function()
+    require('nvim-treesitter.configs').setup {
+        ensure_installed = { "norg", "cpp", "c", "javascript", "python", "lua", "zig", "gitcommit", "git_rebase", "rust" },
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+    }
+end)
