111b71ba791967620a6f354ea1dafccbc6aadb0b
MichaelScofield45 <paco.villamonte@gmail.com>
2024-06-01 22:34:40 -0600

    autocmds: add mode for prosewriting in document formats

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,16 +1,10 @@
-local api = vim.api
-
-api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.norg", "*.md", "*.typ" },
+vim.api.nvim_create_augroup("ProseWriting", {})
+vim.api.nvim_create_autocmd("FileType", {
+  group = "ProseWriting",
+  pattern = { "norg", "markdown", "typst" },
   callback = function()
     vim.bo.textwidth = 80
-    local new_fo, n_subs = string.gsub(vim.bo.formatoptions, "l", "t")
-    if n_subs ~= 0 then
-      vim.bo.formatoptions = new_fo
-    else
-      if not string.match(vim.bo.formatoptions, "t") then
-        vim.bo.formatoptions = vim.bo.formatoptions .. "t"
-      end
-    end
-  end
+    vim.bo.formatoptions = "jcroqt"
+    vim.keymap.set('n', '<leader>fp', 'gwip', {buffer = 0})
+  end,
 })
