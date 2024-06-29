8d835075f9f98bc5aacd5d450b0ceec2600f336b
MichaelScofield45 <paco.villamonte@gmail.com>
2024-06-28 23:20:45 -0600

    aucmds: simplify prose writing aucommand

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,16 +1,8 @@
 local api = vim.api
 
-api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.norg", "*.md", "*.typ" },
+api.nvim_create_autocmd("FileType", {
+  pattern = { "neorg", "markdown", "typst" },
   callback = function()
-    vim.bo.textwidth = 80
-    local new_fo, n_subs = string.gsub(vim.bo.formatoptions, "l", "t")
-    if n_subs ~= 0 then
-      vim.bo.formatoptions = new_fo
-    else
-      if not string.match(vim.bo.formatoptions, "t") then
-        vim.bo.formatoptions = vim.bo.formatoptions .. "t"
-      end
-    end
+      vim.bo.formatoptions = "jqcna"
   end
 })
