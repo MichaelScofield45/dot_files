438bead63e7e11be636f2d21e83c83b81b3c97e2
MichaelScofield45 <paco.villamonte@gmail.com>
2025-02-08 20:27:47 -0600

    Create autocmd for files that need two space indenting

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -5,6 +5,17 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.bo.textwidth = 80
     vim.bo.formatoptions = "jroqa"
-    vim.keymap.set('n', '<leader>fp', 'gwip', {buffer = 0})
+    vim.keymap.set('n', '<localleader>fp', 'gwip', {buffer = 0})
+  end,
+})
+
+vim.api.nvim_create_augroup("TwoSpaceIndent", {})
+vim.api.nvim_create_autocmd("FileType", {
+  group = "TwoSpaceIndent",
+  pattern = { "nix", "lua" },
+  callback = function()
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 0
   end,
 })
