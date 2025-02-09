782b2f300f17d9453c762b5b469d07e4433cf470
MichaelScofield45 <paco.villamonte@gmail.com>
2025-02-08 20:27:27 -0600

    Re-enable blink.cmp, remap keys to use leader key

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
             local servers = { "zls" }
 
             for _, lsp in ipairs(servers) do
-                local capabilities = require("blink.cmp").get_lsp_capabilities()
+                -- local capabilities = require("blink.cmp").get_lsp_capabilities()
                 lspconfig[lsp].setup({capabilities = capabilities})
             end
 
@@ -19,12 +19,12 @@ return {
                     -- Buffer local mappings.
                     -- See `:help vim.lsp.*` for documentation on any of the below functions
                     local opts = { buffer = ev.buf }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
                     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
                     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
                     vim.keymap.set('n', '<leader>f', function()
                         vim.lsp.buf.format { async = true }
                     end, opts)
