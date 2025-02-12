4a8c413de5915376a6b5b7461246cf150268a583
MichaelScofield45 <paco.villamonte@gmail.com>
2025-02-11 21:33:02 -0600

    Reformat to two-space indent

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,37 +1,37 @@
 return {
-    {
-        "echasnovski/mini.nvim",
-        lazy = false,
-        priority = 900,
-        config = function()
-            require("mini.statusline").setup()
-            require("mini.ai").setup()
-            require("mini.align").setup()
-            require("mini.comment").setup()
-            require("mini.surround").setup()
-            require("mini.jump").setup()
-            require("mini.pairs").setup()
-            require("mini.splitjoin").setup()
-            require("mini.pick").setup()
-            require("mini.extra").setup()
-            require("mini.trailspace").setup()
-            require("mini.operators").setup()
-            require("mini.icons").setup()
-            require("mini.move").setup()
-        end,
-        keys = {
-            -- Pick
-            { '<leader>ff', '<cmd>Pick files<cr>' },
-            { '<leader>fg', '<cmd>Pick grep_live<cr>' },
-            { '<leader>fb', '<cmd>Pick buffers<cr>' },
-            { '<leader>fh', '<cmd>Pick history<cr>' },
-            { '<leader>fs', '<cmd>Pick help<cr>' },
-            { '<leader>fc', '<cmd>Pick commands<cr>' },
-            { '<leader>fk', '<cmd>Pick keymaps<cr>' },
+  {
+    "echasnovski/mini.nvim",
+    lazy = false,
+    priority = 900,
+    config = function()
+      require("mini.statusline").setup()
+      require("mini.ai").setup()
+      require("mini.align").setup()
+      require("mini.comment").setup()
+      require("mini.surround").setup()
+      require("mini.jump").setup()
+      require("mini.pairs").setup()
+      require("mini.splitjoin").setup()
+      require("mini.pick").setup()
+      require("mini.extra").setup()
+      require("mini.trailspace").setup()
+      require("mini.operators").setup()
+      require("mini.icons").setup()
+      require("mini.move").setup()
+    end,
+    keys = {
+      -- Pick
+      { '<leader>ff', '<cmd>Pick files<cr>' },
+      { '<leader>fg', '<cmd>Pick grep_live<cr>' },
+      { '<leader>fb', '<cmd>Pick buffers<cr>' },
+      { '<leader>fh', '<cmd>Pick history<cr>' },
+      { '<leader>fs', '<cmd>Pick help<cr>' },
+      { '<leader>fc', '<cmd>Pick commands<cr>' },
+      { '<leader>fk', '<cmd>Pick keymaps<cr>' },
 
-            -- Trailspace
-            { '<leader>tr', function() MiniTrailspace.trim() end },
-            { '<leader>tl', function() MiniTrailspace.trim_last_lines() end }
-        }
+      -- Trailspace
+      { '<leader>tr', function() MiniTrailspace.trim() end },
+      { '<leader>tl', function() MiniTrailspace.trim_last_lines() end }
     }
+  }
 }
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,25 +1,18 @@
 return {
-    {
-        "nvim-neorg/neorg",
-        lazy = false,
-        version = "*",
-        config = function()
-            require("neorg").setup({
-                load = {
-                    ["core.defaults"] = {},
-                    ["core.concealer"] = {},
-                    ["core.keybinds"] = {},
-                    ["core.ui.calendar"] = {},
-                    ["core.dirman"] = {
-                        config = {
-                            workspaces = {
-                                semester = "~/Documents/Feb-Jun2024",
-                            }
-                        }
-                    }
-                }
-            })
-            vim.wo.conceallevel = 2
-        end
-    }
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    version = "*",
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.keybinds"] = {},
+          ["core.ui.calendar"] = {},
+        }
+      })
+      vim.wo.conceallevel = 2
+    end,
+  }
 }
