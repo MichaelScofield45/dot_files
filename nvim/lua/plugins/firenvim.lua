44a983d6aaf28ef550bd94f9d275fced3ee4109c
MichaelScofield45 <paco.villamonte@gmail.com>
2025-03-02 23:04:10 -0600

    Add mini.move and mini.jump back into the mix

--- nvim/lua/plugins/firenvim.lua
+++ nvim/lua/plugins/firenvim.lua
@@ -2,7 +2,7 @@ local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 later(function()
   add({
-    source = 'qlacambre/firenvim',
+    source = 'glacambre/firenvim',
+    -- hooks = { post_install = function() vim.cmd('call firenvim#install(0)') end }
   })
-  -- vim.cmd([[:call firenvim#install(0)]])
 end)
