e574df63498ebeacd6f39be26661ec0788c3d772
MichaelScofield45 <paco.villamonte@gmail.com>
2025-05-08 21:46:25 -0600

    Start using kanso, looks nice

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,7 @@
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 now(function()
-  require('mini.hues').setup({
-    background = '#2b1a33',
-    foreground = '#c9c5cb',
-    saturation = 'lowmedium',
-  })
-  require('mini.colors').setup()
-  MiniColors.get_colorscheme():add_transparency({ statuscolumn = true }):add_cterm_attributes():apply()
+  add({ source = 'webhooked/kanso.nvim' })
+  require('kanso').setup()
+  vim.cmd([[colorscheme kanso]])
 end)
