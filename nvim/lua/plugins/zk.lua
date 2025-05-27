ebb60481b2613f5f9b8e044352afc2ce1f8c2984
MichaelScofield45 <paco.villamonte@gmail.com>
2025-05-26 23:32:19 -0600

    Configure zk plugin

--- nvim/lua/plugins/zk.lua
+++ nvim/lua/plugins/zk.lua
@@ -2,5 +2,17 @@ local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 later(function()
   add({ source = 'zk-org/zk-nvim' })
-  require('zk').setup()
+  require('zk').setup({
+    picker = 'minipick',
+    lsp = {
+      config = {
+        name = "zk",
+        cmd = { "zk", "lsp" },
+        filetypes = { "markdown" },
+      },
+      auto_attach = {
+        enabled = true,
+      },
+    },
+  })
 end)
