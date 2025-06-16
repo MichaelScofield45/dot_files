d49836131151cb5c7e0cb738f504a32546f78dcf
MichaelScofield45 <paco.villamonte@gmail.com>
2025-06-16 16:48:46 -0600

    Remove nui.nvim as dependency, not used

--- nvim/lua/plugins/hardtime.lua
+++ nvim/lua/plugins/hardtime.lua
@@ -3,7 +3,6 @@ local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 later(function()
   add({
     source = 'm4xshen/hardtime.nvim',
-    depends = { 'MunifTanjim/nui.nvim' },
   })
 
   require('hardtime').setup()
