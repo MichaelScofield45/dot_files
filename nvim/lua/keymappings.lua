8a8bc35377f2cfc3188c3495e9877634097b070a
MichaelScofield45 <paco.villamonte@gmail.com>
2025-07-17 00:01:42 -0600

    These are now in default neovim since 0.11

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -1,7 +1,5 @@
 -- My Stuff
 local myfuncs = require("myfuncs")
--- vim.keymap.set("n", "<leader>[", myfuncs.write_new_line_before)
--- vim.keymap.set("n", "<leader>]", myfuncs.write_new_line_after)
 
 vim.api.nvim_create_user_command("SetCompileCommand", myfuncs.set_makeprg, {})
 vim.keymap.set("n", "<leader>cc", function ()
