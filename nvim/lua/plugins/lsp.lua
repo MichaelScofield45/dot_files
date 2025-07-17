40b775bbd2761865e35b448a1b7c076ed19fd4a9
MichaelScofield45 <paco.villamonte@gmail.com>
2025-07-17 00:01:16 -0600

    Update lsp setup since the changes in 0.11

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,6 +1,13 @@
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
+vim.lsp.enable('zls')
 vim.lsp.enable('tinymist')
+vim.lsp.enable('kotlin-language-server')
+vim.lsp.enable('dartls')
+
+vim.diagnostic.config({
+  virtual_text = true,
+})
 
 now(function()
   add({
@@ -17,6 +24,7 @@ now(function()
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
       vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', '<leader>grn', vim.lsp.buf.rename, opts)
       vim.keymap.set('n', '<leader>f', function()
         vim.lsp.buf.format { async = true }
       end, opts)
