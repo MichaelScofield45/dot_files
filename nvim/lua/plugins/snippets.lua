99739e1fc74f1af08bc46c9b1a516bad56d53194
MichaelScofield45 <paco.villamonte@gmail.com>
2025-08-12 10:39:03 -0600

    Remove default snippets, I really do not need them

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -1,10 +1,6 @@
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 local gen_loader = require('mini.snippets').gen_loader
 
-now(function()
-  add({ source = 'rafamadriz/friendly-snippets' })
-end)
-
 local lang_patterns = {
   markdown_inline = { 'markdown.json' },
 }
